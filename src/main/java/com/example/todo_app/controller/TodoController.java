ff93e168d728ec225cbce57eb91c85719b29a9e7
hardik174002 <hvhardik0@gmail.com>
2025-09-11 04:17:15 -0700

    updates

--- src/main/java/com/example/todo_app/controller/TodoController.java
+++ src/main/java/com/example/todo_app/controller/TodoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173","http://frontend:3000", "http://frontend:5173"})
 public class TodoController {
 
     private TodoService todoService;
